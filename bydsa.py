f201209eedb2868eb4c138e303a1b74d1d2098a1
jvillazmx <jvillazmx@gmail.com>
2025-05-22 10:01:16 -0600

    bydsa.py

--- bydsa.py
+++ bydsa.py
@@ -1,6 +1,12 @@
+import gspread
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 import streamlit as st
-import os
 
-st.title("Cuenta de servicio activa")
-st.write("Esta es la cuenta con la que se ejecuta el entorno de Streamlit:")
-st.code(os.environ.get("GOOGLE_SERVICE_ACCOUNT_EMAIL", "No definida"))
+try:
+    sa = gspread.service_account()
+    sh = sa.open_by_key("1JiJttb7OfQIDAVlSCsymZP2r1bn7BOOnVLL89mYo2aY")
+    st.success("¡Acceso exitoso!")
+except Exception as e:
+    st.error(f"ERROR al intentar acceder: {e}")
+
